cbe2d1b4106879dc48b3eca9b0a5897aff2c3e3f
RSK <github@i.uso8000k.net>
2017-08-11 02:27:11 +0900

    fix: fullroute test

--- gobgp/scripts/fullroute.sh
+++ gobgp/scripts/fullroute.sh
@@ -3,7 +3,7 @@
 # download RIB
 cd /opt/
 /usr/bin/wget http://archive.routeviews.org/route-views.wide/bgpdata/`date -d "1 day ago" "+%Y.%m"`/RIBS/rib.`date -d "1 day ago" "+%Y%m%d"`.0000.bz2 -O rib.bz2
-/usr/bunzip2  rib.bz2
+/bin/bunzip2  rib.bz2
 
 $GOPATH/bin/gobgp mrt inject grobal rib
 $GOPATH/bin/gobgp global rib 8.8.8.8
